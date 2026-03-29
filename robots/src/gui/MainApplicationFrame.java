1e2a2f212a1c1ee5c3195dc936f87b3d7bb513c6
bagdan.sharapov@gmail.com <festena@Bsh.localdomain>
2026-03-29 21:23:39 +0500

    Рефактор MainApplicationFrame: устранил дублирование кода, разделил логику в конструкторе, разбил метод создания меню на несколько отдельных методов

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -2,47 +2,54 @@ package gui;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
 import java.util.Locale;
 
 import javax.swing.*;
 
 import log.Logger;
 
-/**
- * Что требуется сделать:
- * 1. Метод создания меню перегружен функционалом и трудно читается. 
- * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
- *
- */
 public class MainApplicationFrame extends JFrame
 {
     private final JDesktopPane desktopPane = new JDesktopPane();
-    
-    public MainApplicationFrame() {
-        //Make the big window be indented 50 pixels from each edge
-        //of the screen.
-        Locale.setDefault(new Locale("ru", "RU"));
 
-        int inset = 50;        
+    private static final String MENU_LOOK_AND_FEEL = "Режим отображения";
+    private static final String MENU_TEST = "Тесты";
+    private static final String MENU_EXIT = "Выход";
+
+
+    
+    public MainApplicationFrame(){
+        setLocale("ru", "RU");
+        setupMainWindow();
+        initDesktop();
+        initWindows();
+        setJMenuBar(createMenuBar());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    // вынес в отдельный метод чтобы в будущем была возможность
+    // улучшить локализацию
+    private void setLocale(String lang, String country){
+        Locale.setDefault(new Locale(lang, country));
+    }
+
+    // главное окна
+    private void setupMainWindow() {
+        int inset = 50;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset,
-            screenSize.width  - inset*2,
-            screenSize.height - inset*2);
+                screenSize.width - inset * 2,
+                screenSize.height - inset * 2);
+    }
 
+    private void initDesktop() {
         setContentPane(desktopPane);
-        
-        
-        LogWindow logWindow = createLogWindow();
-        addWindow(logWindow);
+    }
 
-        GameWindow gameWindow = new GameWindow();
-        gameWindow.setSize(400,  400);
-        addWindow(gameWindow);
-
-        setJMenuBar(generateMenuBar());
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    private void initWindows() {
+        addWindow(createLogWindow());
+        addWindow(createGameWindow());
     }
     
     protected LogWindow createLogWindow()
@@ -55,100 +62,63 @@ public class MainApplicationFrame extends JFrame
         Logger.debug("Протокол работает");
         return logWindow;
     }
-    
+
+    protected GameWindow createGameWindow(){
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.setSize(400, 400);
+        return gameWindow;
+    }
+
+    // метод создания меню
+    private JMenuBar createMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(createLookAndFeelMenu());
+        menuBar.add(createTestMenuItem());
+        menuBar.add(createExitMenuItem());
+        return menuBar;
+    }
+
     protected void addWindow(JInternalFrame frame)
     {
         desktopPane.add(frame);
         frame.setVisible(true);
     }
-    
-//    protected JMenuBar createMenuBar() {
-//        JMenuBar menuBar = new JMenuBar();
-// 
-//        //Set up the lone menu.
-//        JMenu menu = new JMenu("Document");
-//        menu.setMnemonic(KeyEvent.VK_D);
-//        menuBar.add(menu);
-// 
-//        //Set up the first menu item.
-//        JMenuItem menuItem = new JMenuItem("New");
-//        menuItem.setMnemonic(KeyEvent.VK_N);
-//        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-//                KeyEvent.VK_N, ActionEvent.ALT_MASK));
-//        menuItem.setActionCommand("new");
-////        menuItem.addActionListener(this);
-//        menu.add(menuItem);
-// 
-//        //Set up the second menu item.
-//        menuItem = new JMenuItem("Quit");
-//        menuItem.setMnemonic(KeyEvent.VK_Q);
-//        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-//                KeyEvent.VK_Q, ActionEvent.ALT_MASK));
-//        menuItem.setActionCommand("quit");
-////        menuItem.addActionListener(this);
-//        menu.add(menuItem);
-// 
-//        return menuBar;
-//    }
-    
-    private JMenuBar generateMenuBar()
-    {
-        JMenuBar menuBar = new JMenuBar();
-        
-        JMenu lookAndFeelMenu = new JMenu("Режим отображения");
-        lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
-        lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
-                "Управление режимом отображения приложения");
-        
-        {
-            JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
-            systemLookAndFeel.addActionListener((event) -> {
-                setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                this.invalidate();
-            });
-            lookAndFeelMenu.add(systemLookAndFeel);
-        }
 
-        {
-            JMenuItem crossplatformLookAndFeel = new JMenuItem("Универсальная схема", KeyEvent.VK_S);
-            crossplatformLookAndFeel.addActionListener((event) -> {
-                setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-                this.invalidate();
-            });
-            lookAndFeelMenu.add(crossplatformLookAndFeel);
-        }
+    private JMenu createLookAndFeelMenu() {
+        JMenu menu = new JMenu(MENU_LOOK_AND_FEEL);
 
-        JMenu testMenu = new JMenu("Тесты");
-        testMenu.setMnemonic(KeyEvent.VK_T);
-        testMenu.getAccessibleContext().setAccessibleDescription(
-                "Тестовые команды");
-        
-        {
-            JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
-            addLogMessageItem.addActionListener((event) -> {
-                Logger.debug("Новая строка");
-            });
-            testMenu.add(addLogMessageItem);
-        }
+        menu.add(createMenuItem("Системная схема",
+                e -> setLookAndFeel(UIManager.getSystemLookAndFeelClassName())));
 
-        JMenu exitMenu = new JMenu("Закрыть окно");
-        testMenu.setMnemonic(KeyEvent.VK_T);
-        testMenu.getAccessibleContext().setAccessibleDescription(
-                "Выход");
+        menu.add(createMenuItem("Универсальная схема",
+                e -> setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName())));
 
-        {
-            JMenuItem addLogMessageItem = new JMenuItem("Выход из приложения", KeyEvent.VK_S);
-            addLogMessageItem.addActionListener((event) -> {
-                Logger.debug("Кнопка выхода нажата");
-                exitCommand();
-            });
-            exitMenu.add(addLogMessageItem);
-        }
+        return menu;
+    }
 
-        menuBar.add(lookAndFeelMenu);
-        menuBar.add(testMenu);
-        menuBar.add(exitMenu);
-        return menuBar;
+    private JMenu createTestMenuItem() {
+        JMenu menu = new JMenu(MENU_TEST);
+
+        menu.add(createMenuItem("Сообщение в лог",
+                e -> Logger.debug("Новая строка")));
+
+        return menu;
+    }
+
+    private JMenu createExitMenuItem() {
+        JMenu menu = new JMenu(MENU_EXIT);
+
+        menu.add(createMenuItem("Закрыть приложение",
+                e -> exitCommand()));
+
+        return menu;
+    }
+
+    // универсальный метод создания пункта меню -дублирование
+    private JMenuItem createMenuItem(String title, ActionListener action) {
+        JMenuItem item = new JMenuItem(title);
+        item.addActionListener(action);
+        return item;
     }
     
     private void setLookAndFeel(String className)
